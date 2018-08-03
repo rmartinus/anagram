9a54c7a8c44e88ecb115fe9c8bd20d6b9bc313b8
Robbie Martinus <robbie.martinus@qantasloyalty.com>
2018-08-03 16:34:55 +1000

    Implement anagram generation logic

--- src/test/java/anagram/Anagram.java
+++ src/test/java/anagram/Anagram.java
@@ -1,13 +1,34 @@
 package anagram;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Anagram {
-    /**
-     * ab -> ab, ba
-     */
-    public static List<String> generate(String string) {
-        return Collections.singletonList(string);
+    public static Set<String> generate(String string) {
+        Set<String> result = new HashSet<>();
+        traverse(string, 0, string.length() - 1, result);
+        return result;
+    }
+
+    private static void traverse(String string, int startIndex, int endIndex, Set<String> permutation) {
+        if (startIndex == endIndex) {
+            permutation.add(string);
+        } else {
+            for (int i = startIndex; i <= endIndex; i++) {
+                string = swap(string, startIndex, i);
+                traverse(string, startIndex + 1, endIndex, permutation);
+                string = swap(string, startIndex, i);
+            }
+        }
+    }
+
+    public static String swap(String string, int sourceIndex, int targetIndex) {
+        char tempChar;
+        char[] chars = string.toCharArray();
+        tempChar = chars[sourceIndex];
+        chars[sourceIndex] = chars[targetIndex];
+        chars[targetIndex] = tempChar;
+
+        return String.valueOf(chars);
     }
 }
